f0c16944799e2bac5d89f333fb034a60e77faa51
YukinariSatoJTP <sato1972@jtp.co.jp>
2025-06-24 16:07:33 +0900

    Added unit tests

--- cdk-cicd/test/cdk-cicd.test.ts
+++ cdk-cicd/test/cdk-cicd.test.ts
@@ -1,4 +1,10 @@
+import { handler } from "../service/hello"
 
-test('initial test', () => {
 
-});
+
+describe("Hello describe test suite", () => {
+  test("handler should return 200", async () => {
+    const result = await handler({}, {});
+    expect(result.statusCode).toBe(200);
+  });
+})
